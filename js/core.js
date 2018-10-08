a3a11bfe137b19f12b822ed067f87fb1538d8d93
Emmanuel Vinicius <emmanuel.vinicius.98@gmail.com>
2018-10-08 16:03:20 -0300

    Alterações com conteudo

--- js/core.js
+++ js/core.js
@@ -1,5 +1,33 @@
+//Remover acentos
+function removerAcentos(newStringComAcento) {
+    var string = newStringComAcento;
+    var mapaAcentosHex = {
+        a: /[\xE0-\xE6]/g,
+        A: /[\xC0-\xC6]/g,
+        e: /[\xE8-\xEB]/g,
+        E: /[\xC8-\xCB]/g,
+        i: /[\xEC-\xEF]/g,
+        I: /[\xCC-\xCF]/g,
+        o: /[\xF2-\xF6]/g,
+        O: /[\xD2-\xD6]/g,
+        u: /[\xF9-\xFC]/g,
+        U: /[\xD9-\xDC]/g,
+        c: /\xE7/g,
+        C: /\xC7/g,
+        n: /\xF1/g,
+        N: /\xD1/g,
+    };
+
+    for (var letra in mapaAcentosHex) {
+        var expressaoRegular = mapaAcentosHex[letra];
+        string = string.replace(expressaoRegular, letra);
+    }
+
+    return string;
+}
+
 // Sticky Header
-$(window).scroll(function() {    
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 100) {
@@ -9,17 +37,30 @@ $(window).scroll(function() {
     }
 });
 
-// Year for copy content
-$(function(){
-var theYear = new Date().getFullYear();
-$('#year').html(theYear);
-});
+$(document.body).on("click", ".col-lg-4", function (ev) {
+    $(this).attr("data-toggle", "modal").attr("data-target", "#exampleModal");
+    var servico = $(ev.target);
+    var valorServico = servico.children("h3");
+    var nomeServico = valorServico.text();
+    if (nomeServico == "") {
+        valorServico = servico.siblings("h3");
+    }
+    nomeServico = valorServico.text();
+    $(".modal-title").text(nomeServico);
+    var tituloAjax = removerAcentos(nomeServico);
 
-// Google Map
-function initMap() {
-    var location = {lat: 40.730, lng: -73.935};
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom:13,
-      center:location
-    });
-  }
+    $.ajax({
+        url: "ajax/" + tituloAjax + ".html",
+        type: "GET",
+        dataType: "text",
+        success: function (result) {
+            $(".modal-body").html(result);
+        }
+    })
+})
+
+// Year for copy content
+$(function () {
+    var theYear = new Date().getFullYear();
+    $('#year').html(theYear);
+});
